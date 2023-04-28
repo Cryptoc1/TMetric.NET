2c41cc84956c694b3f9e4e9c1fd329bb5ab2434c
Samuel Steele <steelesam72@gmail.com>
2023-04-28 11:20:47 -0400

    Added test to ensure Client can be constructed via DI

---
+++ src/TMetric/test/ServiceExtensionTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TMetric.Abstractions;
+
+namespace TMetric.Tests;
+
+public sealed class ServiceExtensionTests
+{
+    [Fact]
+    public void Client_can_be_constructed( )
+    {
+        var services = new ServiceCollection()
+            .AddTMetric( options => options.ApiKey = "TEST" )
+            .BuildServiceProvider();
+
+        var exception = Record.Exception( services.GetRequiredService<ITMetricClient> );
+
+        Assert.Null( exception );
+        Assert.IsNotType<InvalidOperationException>( exception );
+    }
+}
